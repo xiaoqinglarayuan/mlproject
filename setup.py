702f4783b3b075845f61e4e8147f3913ddabeb54
Lara <xyuan93@gatech.edu>
2025-12-12 12:31:32 -0800

    setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)-> list:
+    ''''''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+ 
+
+setup(
+    name = 'mlproject',
+    version = '0.0.1',
+    author = 'Lara', 
+    author_email = 'xyuan93@gatech.edu',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
+
